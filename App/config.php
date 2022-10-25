50a592a17fac8fe91f4e2febf177218ce5cf8b57
Lucas A <83725986+smokeeaasd@users.noreply.github.com>
2022-10-25 09:50:00 -0300

    Update config.php

--- App/config.php
+++ App/config.php
@@ -5,7 +5,7 @@ define('VIEWS', BASEDIR . '\\App\\View\\modules\\');
 
 $_ENV['db']['host'] = 'localhost:3307';
 $_ENV['db']['user'] = 'root';
-$_ENV['db']['pass'] = 'Lcs6141$';
+$_ENV['db']['pass'] = 'senha';
 $_ENV['db']['database'] = 'db_sourcecode';
 
 ?>
